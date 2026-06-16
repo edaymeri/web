0021c14208a16de669f310a25fcbeaceede4fdd5
edaymeri <ymerieda2010@gmail.com>
2026-06-16 17:47:44 +0200

    mm

---
+++ model27/js/main.js
@@ -0,0 +1,11 @@
+function kthesms(){
+    alert("ky eshte nje funksion qe kthen nje sms")
+}
+
+kthesms();
+
+
+function sum(num1,num2,num3){//parametra qe na ndihmojn per ti treguar funksionit qfar vlera pret
+    return num1+num2+num3 ;
+}
+ console.log (sum(2,5,7));
---
+++ module26/js/main.js
@@ -0,0 +1,61 @@
+console.log("hello from eda")
+
+var x=5;
+
+var y=28+43;
+console.log(y);
+
+var b=64%21;
+console.log(b);
+
+var c=99;
+c++;
+console.log(c);
+
+var c=99;
+c--;
+console.log(c);
+
+var q=4;
+var p=q**2;
+console.log(p);
+
+
+var b=6;
+b*=2;
+console.log(b);
+
+var b=6;
+var a=7;
+console.log(a==b);
+
+
+
+console.log("-------------");
+
+var b=7;
+var a="7";
+console.log(a===b);
+
+var b=6;
+var a=7;
+console.log(a>b);
+
+var b=6;
+var a=74;
+console.log(a<b);
+
+var b=6;
+var a=6;
+console.log(a!=b);
+
+var name="eda";
+var lastname="ymeri";
+var fullname= name+ " " + lastname;
+console.log(fullname);
+
+var age=24;
+console.log(age>=18 && age<=65) //and duhen te dy kushtet tjen te sakta
+
+var age=23;
+console.log(age <=22 || age<=65)//an mjafton njeri prej kushteve te  jet i sakt
